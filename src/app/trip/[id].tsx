a2c4253197fd5e3d64e029d08d095a4da0b16c7e
Kenzo Sato <kenzohideaky@gmail.com>
2024-08-04 17:59:16 -0300

    feat: finished journey

--- src/app/trip/[id].tsx
+++ src/app/trip/[id].tsx
@@ -67,6 +67,10 @@ export default function Trip() {
     try {
       setIsLoadingTrip(true);
 
+      if (tripParams.participant) {
+        setShowModal(MODAL.CONFIRM_ATTENDENCE);
+      }
+
       if (!tripParams.id) {
         return router.back();
       }
@@ -179,6 +183,23 @@ export default function Trip() {
     }
   };
 
+  const handleRemoveTrip = async () => {
+    try {
+      Alert.alert("Remover viagem", "Tem certeza que deseja remover a viage?", [
+        { text: "Não", style: "cancel" },
+        {
+          text: "Sim",
+          onPress: async () => {
+            await tripStorage.remove();
+            router.navigate("/");
+          },
+        },
+      ]);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   useEffect(() => {
     getTripDetails();
   }, []);
@@ -267,6 +288,12 @@ export default function Trip() {
           <Button onPress={handleUpdateTrip} isLoading={isUpdatingTrip}>
             <Button.Title>Atualizar</Button.Title>
           </Button>
+
+          <TouchableOpacity activeOpacity={0.8} onPress={handleRemoveTrip}>
+            <Text className="text-red-400 text-center mt-6">
+              Remover da viagem
+            </Text>
+          </TouchableOpacity>
         </View>
       </Modal>
 
